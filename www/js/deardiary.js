85e3fbc54499a1170e9e02ad8538d88e1b353c70
anezasabdani <30885974+anezasabdani@users.noreply.github.com>
2017-10-27 17:28:34 +0800

    Add files via upload

---
+++ www/js/deardiary.js
@@ -0,0 +1,22 @@
+document.getElementById('button_save').addEventListener('click', function(){
+    let textarea = document.getElementById('diary_input').value;
+    if(textarea != ""){
+        let testCount = 0;
+        if(localStorage.getItem('test-count') == null){
+            localStorage.setItem('test-count',0);
+            testCount = 0;
+        }
+        else
+            testCount = localStorage.getItem('test-count');
+        localStorage.setItem('test-'+((testCount*1)+1), textarea);
+        localStorage.setItem('test-count' , ((testCount*1)+1));
+        let testCountNumber = (testCount*1)+1;
+        let content = "";
+        for(let a = 1; a<=testCountNumber;a++){
+            content += "<li>"+ localStorage.getItem('test-'+a)+"</li>"
+            ;
+        }
+        document.getElementById('diary_list').innerHTML = content;
+        document.getElementById('diary_input').value = "";
+    }
+});
